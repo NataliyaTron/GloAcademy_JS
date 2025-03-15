fd97129dba719b9284dc5bbf212671ddd45b73a9
Nataliya Trondina <trondina.nataliya@yandex.ru>
2025-03-15 17:09:21 +0400

    выполнено первое задание

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("Hello, students GloAcademy!");
+
+console.log("Good luck with your Git learning!");
